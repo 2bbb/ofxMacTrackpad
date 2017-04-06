bf306c8a6e2b0d1d0ea03e64780425bc7104ae35
ISHII 2bit <ishii@buffer-renaiss.com>
2017-04-06 18:12:41 +0900

    initial

---
+++ src/ofxMacTrackpad.h
@@ -0,0 +1,123 @@
+//
+//  ofxMacTrackpad.hpp
+//
+//  Created by ISHII 2bit on 2017/04/04.
+//
+//
+
+#ifndef ofxMacTrackpad_h
+#define ofxMacTrackpad_h
+
+#include "ofMain.h"
+
+namespace ofx {
+    namespace MacTrackpad {
+        enum class EventPhase : std::uint8_t {
+            None       = 0,
+            Began      = 0x1 << 0,
+            Stationary = 0x1 << 1,
+            Changed    = 0x1 << 2,
+            Ended      = 0x1 << 3,
+            Cancelled  = 0x1 << 4,
+            MayBegin   = 0x1 << 5
+        };
+        
+        struct BaseArg {
+            EventPhase phase;
+            double timestamp;
+        };
+        
+        enum class TouchPhase : std::uint8_t {
+            Began = 1 << 0,
+            Moved = 1 << 1,
+            Stationary = 1 << 2,
+            Ended = 1 << 3,
+            Cancelled = 1 << 4,
+            Touching = Began | Moved | Stationary,
+            Any = 255
+        };
+        
+        struct TouchedFinger {
+            TouchPhase phase;
+            std::uint64_t identity;
+            ofPoint position;
+            ofVec2f delta;
+            bool isResting;
+        };
+        
+        struct TouchArg : BaseArg {
+            std::vector<TouchedFinger> fingers;
+            std::vector<TouchedFinger> releasedFingers;
+        };
+        
+        enum class PressureBehavior : std::int8_t {
+            Unknown = -1,
+            PrimaryDefault = 0,
+            PrimaryClick = 1,
+            PrimaryGeneric = 2,
+            PrimaryAccelerator = 3,
+            PrimaryDeepClick = 5,
+            PrimaryDeepDrag = 6
+        };
+        
+        struct PressureArg : BaseArg {
+            float pressure;
+            PressureBehavior behavior;
+            std::uint8_t stage;
+            float stageTransition;
+            
+            inline float normalizedPressure() const {
+                return (phase == EventPhase::Ended) ? 0.0f : ofClamp(0.5f * (pressure + stage - 1), 0.0f, 1.0f);
+            }
+        };
+        
+        struct PinchArg : BaseArg {
+            float magnification;
+        };
+        
+        struct RotateArg : BaseArg {
+            float rotation;
+        };
+        
+        struct SwipeArg : BaseArg {
+            ofPoint delta;
+        };
+        
+        struct TwoFingerDoubleTapArg : BaseArg {};
+        
+        extern ofEvent<TouchedFinger> touch;
+        extern ofEvent<TouchedFinger> release;
+        extern ofEvent<TouchArg> multitouch;
+        
+        extern ofEvent<TouchedFinger> touchTouchbar;
+        extern ofEvent<TouchedFinger> releaseTouchbar;
+        extern ofEvent<TouchArg> multitouchTouchbar;
+        
+        extern ofEvent<PressureArg> pressure;
+        extern ofEvent<PinchArg> pinch;
+        extern ofEvent<RotateArg> rotate;
+        extern ofEvent<SwipeArg> swipe;
+        extern ofEvent<TwoFingerDoubleTapArg> twoFingerDoubleTap;
+        
+        void startListening();
+        void stopListening();
+    };
+};
+
+namespace ofxMacTrackpad = ofx::MacTrackpad;
+
+using ofxMacTrackpadEventPhase = ofxMacTrackpad::EventPhase;
+
+using ofxMacTrackpadTouchPhase = ofxMacTrackpad::TouchPhase;
+using ofxMacTrackpadTouchedFinger = ofxMacTrackpad::TouchedFinger;
+using ofxMacTrackpadTouchArg = ofxMacTrackpad::TouchArg;
+
+using ofxMacTrackpadPressureBehavior = ofxMacTrackpad::PressureBehavior;
+using ofxMacTrackpadPressureArg = ofxMacTrackpad::PressureArg;
+
+using ofxMacTrackpadPinchArg = ofxMacTrackpad::PinchArg;
+using ofxMacTrackpadRotateArg = ofxMacTrackpad::RotateArg;
+using ofxMacTrackpadSwipeArg = ofxMacTrackpad::SwipeArg;
+using ofxMacTrackpadTwoFingerDoubleTapArg = ofxMacTrackpad::TwoFingerDoubleTapArg;
+
+#endif /* ofxMacTrackpad_h */
---
+++ src/ofxMacTrackpad.mm
@@ -0,0 +1,259 @@
+//
+//  ofxMacTrackpad.mm
+//
+//  Created by ISHII 2bit on 2017/04/04.
+//
+//
+
+#include "ofxMacTrackpad.h"
+
+#import <AppKit/AppKit.h>
+#import <ApplicationServices/ApplicationServices.h>
+
+#ifndef MAC_OS_X_VERSION_10_7
+#   error this addon requires osx 10.7+
+#endif
+
+NSEventMask eventMask   = NSEventMaskGesture
+                        | NSEventMaskBeginGesture
+                        | NSEventMaskEndGesture
+                        | NSEventMaskMagnify
+                        | NSEventMaskSmartMagnify
+                        | NSEventMaskSwipe
+                        | NSEventMaskRotate
+                        | NSEventMaskDirectTouch
+                        | NSEventMaskPressure;
+
+namespace ofx {
+    namespace MacTrackpad {
+        ofEvent<TouchedFinger> touch;
+        ofEvent<TouchedFinger> release;
+        ofEvent<TouchArg> multitouch;
+        
+        ofEvent<TouchedFinger> touchTouchbar;
+        ofEvent<TouchedFinger> releaseTouchbar;
+        ofEvent<TouchArg> multitouchTouchbar;
+        
+        ofEvent<PressureArg> pressure;
+        ofEvent<PinchArg> pinch;
+        ofEvent<RotateArg> rotate;
+        ofEvent<SwipeArg> swipe;
+        ofEvent<TwoFingerDoubleTapArg> twoFingerDoubleTap;
+        
+        CGEventRef eventTapCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef eventRef, void *refcon);
+        bool stopListening(ofEventArgs &arg);
+        CFRunLoopRef runLoop;
+        CFMachPortRef eventTap;
+        CFRunLoopSourceRef runLoopSource;
+        
+        void startListening() {
+            runLoop = CFRunLoopGetCurrent();
+            eventTap = CGEventTapCreate(kCGSessionEventTap,
+                                        kCGHeadInsertEventTap,
+                                        kCGEventTapOptionListenOnly,
+                                        kCGEventMaskForAllEvents,
+                                        eventTapCallback,
+                                        nil);
+            runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
+            CFRunLoopAddSource(runLoop, runLoopSource, kCFRunLoopCommonModes);
+            CGEventTapEnable(eventTap, true);
+            
+            ofAddListener(ofEvents().exit, &stopListening, OF_EVENT_ORDER_BEFORE_APP);
+        }
+        
+        void stopListening() {
+            if(eventTap != NULL) {
+                CGEventTapEnable(eventTap, false);
+            }
+            if(runLoopSource != NULL) {
+                CFRunLoopRemoveSource(runLoop, runLoopSource, kCFRunLoopCommonModes);
+                CFRelease(runLoopSource);
+            }
+        }
+        
+        bool stopListening(ofEventArgs &arg) {
+            stopListening();
+        }
+
+        CGEventRef eventTapCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef eventRef, void *refcon) {
+            static std::map<std::uint64_t, TouchedFinger> currentFingers;
+            static std::map<std::uint64_t, TouchedFinger> currentTouchbarFingers;
+            
+            if(type == kCGEventTapDisabledByUserInput) return eventRef;
+            if(type == kCGEventTapDisabledByTimeout) return eventRef;
+            // type == 30 then NSEvent can't be made from eventRef
+            if(type == 30) return eventRef;
+            NSEvent *event = [NSEvent eventWithCGEvent:eventRef];
+            
+            // filter out events which do not match the mask
+            if(!(eventMask & NSEventMaskFromType(event.type))) return event.CGEvent;
+            
+            switch(event.type) {
+                case NSEventTypeSwipe: {
+                    ofLogNotice() << "Swipe";
+                    SwipeArg arg;
+                    arg.delta.x = event.deltaX;
+                    arg.delta.y = event.deltaY;
+                    arg.phase = static_cast<EventPhase>(event.phase);
+                    arg.timestamp = event.timestamp;
+                    
+                    ofNotifyEvent(ofxMacTrackpad::swipe, arg);
+                    break;
+                }
+                case NSEventTypeRotate: {
+                    RotateArg arg;
+                    arg.rotation = event.rotation;
+                    arg.phase = static_cast<EventPhase>(event.phase);
+                    arg.timestamp = event.timestamp;
+                    
+                    ofNotifyEvent(ofxMacTrackpad::rotate, arg);
+                    break;
+                }
+                case NSEventTypeMagnify: {
+                    PinchArg arg;
+                    arg.magnification = event.magnification;
+                    arg.phase = static_cast<EventPhase>(event.phase);
+                    arg.timestamp = event.timestamp;
+                    
+                    ofNotifyEvent(ofxMacTrackpad::pinch, arg);
+                    break;
+                }
+#ifdef MAC_OS_X_VERSION_10_8
+                case NSEventTypeSmartMagnify: {
+                    TwoFingerDoubleTapArg arg;
+                    arg.phase = static_cast<EventPhase>(event.phase);
+                    arg.timestamp = event.timestamp;
+                    ofNotifyEvent(twoFingerDoubleTap, arg);
+                    break;
+                }
+#endif
+#ifdef MAC_OS_X_VERSION_10_10_3
+                case NSEventTypePressure: {
+                    PressureArg arg;
+                    arg.pressure = event.pressure;
+                    arg.behavior = static_cast<PressureBehavior>(event.pressureBehavior);
+                    arg.stage    = event.stage;
+                    arg.stageTransition = event.stageTransition;
+                    arg.phase    = static_cast<EventPhase>(event.phase);
+                    arg.timestamp = event.timestamp;
+                    
+                    ofNotifyEvent(ofxMacTrackpad::pressure, arg);
+                }
+#endif
+                default:
+                    break;
+            }
+            
+            if(event.type == NSEventTypeGesture) {
+                NSArray<NSTouch *> *touches = event.allTouches.allObjects;
+                TouchArg touchEvent;
+                touchEvent.phase = static_cast<EventPhase>(event.phase);
+                touchEvent.timestamp = event.timestamp;
+                for(std::size_t i = 0, size = touches.count; i < size; i++) {
+                    NSTouch *touch = touches[i];
+                    if(touch.type == NSTouchTypeDirect) {
+                        continue;
+                    }
+                    std::uint64_t identity = reinterpret_cast<std::uint64_t>(touch.identity);;
+                    if(currentFingers.find(identity) != currentFingers.end()) {
+                        TouchedFinger &arg = currentFingers[identity];
+                        NSPoint p = touch.normalizedPosition;
+                        arg.delta.x = p.x - arg.position.x;
+                        arg.delta.y = (1.0f - arg.position.y) - p.y;
+                        
+                        arg.position.x = p.x;
+                        arg.position.y = 1.0f - p.y;
+                        arg.isResting = touch.isResting;
+                        
+                        arg.phase = (touch.phase == NSTouchPhaseAny)
+                                  ? TouchPhase::Any
+                                  : static_cast<TouchPhase>(touch.phase);
+                    } else {
+                        TouchedFinger arg;
+                        arg.identity = identity;
+                        arg.delta.x = 0.0f;
+                        arg.delta.y = 0.0f;
+                        
+                        NSPoint p = touch.normalizedPosition;
+                        arg.position.x = p.x;
+                        arg.position.y = 1.0f - p.y;
+                        arg.isResting = touch.isResting;
+                        
+                        arg.phase = (touch.phase == NSTouchPhaseAny)
+                                  ? TouchPhase::Any
+                                  : static_cast<TouchPhase>(touch.phase);
+                        
+                        currentFingers[identity] = arg;
+                        ofNotifyEvent(ofxMacTrackpad::touch, arg);
+                    }
+                    
+                    if(!(touch.phase & NSTouchPhaseTouching)) {
+                        TouchedFinger &arg = currentFingers[identity];
+                        ofNotifyEvent(release, arg);
+                        touchEvent.releasedFingers.push_back(arg);
+                        currentFingers.erase(identity);
+                    }
+                }
+                
+                for(const auto &it : currentFingers) touchEvent.fingers.push_back(it.second);
+                ofNotifyEvent(multitouch, touchEvent);
+            }
+            
+#ifdef MAC_OS_X_VERSION_10_12_1
+            else if(event.type == NSEventTypeDirectTouch) {
+                // Touchbar
+                NSArray<NSTouch *> *touches = event.allTouches.allObjects;
+                TouchArg touchEvent;
+                touchEvent.phase = static_cast<EventPhase>(event.phase);
+                touchEvent.timestamp = event.timestamp;
+                for(std::size_t i = 0, size = touches.count; i < size; i++) {
+                    NSTouch *touch = touches[i];
+                    std::uint64_t identity = reinterpret_cast<std::uint64_t>(touch.identity);;
+                    if(currentTouchbarFingers.find(identity) != currentTouchbarFingers.end()) {
+                        TouchedFinger &arg = currentTouchbarFingers[identity];
+                        NSPoint p = [touch locationInView:nil];
+                        arg.delta.x = p.x - arg.position.x;
+                        arg.delta.y = (1.0f - arg.position.y) - p.y;
+                        
+                        arg.position.x = p.x;
+                        arg.position.y = 1.0f - p.y;
+                        arg.isResting = touch.isResting;
+                        
+                        arg.phase = (touch.phase == NSTouchPhaseAny)
+                                  ? TouchPhase::Any
+                                  : static_cast<TouchPhase>(touch.phase);
+                    } else {
+                        TouchedFinger arg;
+                        arg.identity = identity;
+                        arg.delta.x = 0.0f;
+                        arg.delta.y = 0.0f;
+                        
+                        NSPoint p = [touch locationInView:nil];;
+                        arg.position.x = p.x;
+                        arg.position.y = 1.0f - p.y;
+                        arg.isResting = touch.isResting;
+                        
+                        arg.phase = (touch.phase == NSTouchPhaseAny)
+                                  ? TouchPhase::Any
+                                  : static_cast<TouchPhase>(touch.phase);
+
+                        currentTouchbarFingers[identity] = arg;
+                        ofNotifyEvent(touchTouchbar, arg);
+                    }
+                    
+                    if(!(touch.phase & NSTouchPhaseTouching)) {
+                        TouchedFinger &arg = currentTouchbarFingers[identity];
+                        ofNotifyEvent(releaseTouchbar, arg);
+                        touchEvent.releasedFingers.push_back(arg);
+                        currentTouchbarFingers.erase(identity);
+                    }
+                }
+                
+                for(const auto &it : currentTouchbarFingers) touchEvent.fingers.push_back(it.second);
+                ofNotifyEvent(multitouchTouchbar, touchEvent);
+            }
+            return event.CGEvent;
+        }
+#endif
+    };
+};
